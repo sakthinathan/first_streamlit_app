279d619e5f314d581b8076e22f3db501bb95bc55
SAKTHINATHAN <58353645+sakthinathan@users.noreply.github.com>
2024-02-20 10:25:50 +0530

    API

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-
-
-# Display the table on the page.
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
